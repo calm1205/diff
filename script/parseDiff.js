832d4820e06685f64f614060da42a3c5495c075a
calm1205 <authentic.nagi@gmail.com>
2024-10-03 09:32:09 +0900

    update

--- script/parseDiff.js
+++ script/parseDiff.js
@@ -8,23 +8,27 @@ const parseDiff = (lines) => {
   let additionCount = 0;
 
   lines.forEach((line) => {
-    if (line.startsWith("-")) {
-      deletionCount++;
-      // '-' 行は元のファイルにのみ存在する
-      originalFile.push(line.slice(1).trim()); // '-' を取り除いて、元のファイルに追加
-    } else if (line.startsWith("+")) {
-      additionCount++;
-      // '+' 行は変更後のファイルにのみ存在する
-      if (additionCount > deletionCount) {
-        originalFile.push(""); // 元のファイルには空行を入れる
-        deletionCount = 0;
-        additionCount = 0;
-      }
-      modifiedFile.push(line.slice(1).trim()); // '+' を取り除いて、変更後のファイルに追加
-    } else {
-      // 変更のない行は両方のファイルにそのまま追加
-      originalFile.push(line.trim());
-      modifiedFile.push(line.trim());
+    const firstChar = line.charAt(0);
+
+    switch (firstChar) {
+      case "-": // '-' 行は元のファイルにのみ存在する
+        deletionCount++;
+        originalFile.push(line.slice(1).trim());
+        break;
+      case "+": // '+' 行は変更後のファイルにのみ存在する
+        additionCount++;
+        if (additionCount > deletionCount) {
+          // 追加行が削除行より多い場合は元のファイルに空行を追加
+          originalFile.push("");
+          deletionCount = 0;
+          additionCount = 0;
+        }
+        modifiedFile.push(line.slice(1).trim());
+        break;
+      default: // 変更のない行は両方のファイルにそのまま追加
+        originalFile.push(line.trim());
+        modifiedFile.push(line.trim());
+        break;
     }
   });
 
