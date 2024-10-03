d3f385d2b9985c58e551e007116b78bdf2627902
calm1205 <authentic.nagi@gmail.com>
2024-10-03 17:29:11 +0900

    feat: setup vite

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import "./style.css"
+// import { setupCounter } from "./counter.ts"
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>Vite + TypeScript</h1>
+  </div>
+`
+
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!)
