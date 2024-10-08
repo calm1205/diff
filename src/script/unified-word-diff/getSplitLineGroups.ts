18ecab933e38adf31726ffaa16cb76846c842f49
calm1205 <authentic.nagi@gmail.com>
2024-10-08 11:16:59 +0900

    fix: insert comment

--- src/script/unified-word-diff/getSplitLineGroups.ts
+++ src/script/unified-word-diff/getSplitLineGroups.ts
@@ -1,14 +1,29 @@
+/**
+ * git diff --word-diffのparse関数
+ *
+ * 修正前(originalLines)と修正後(modifiedLines)に分割する。
+ *
+ * git diffの出力では追加部分は{+...+}で囲まれ、削除部分は[-...-]で囲まれる。
+ *
+ * 修正を以下の3つのケースに分ける。
+ * 1. 文全体が{+ , +}で囲まれている場合は文の追加
+ * 2. 文全体が[- , -]で囲まれている場合は文の削除
+ * 3. 文の途中が{+ , +}や[-, -]で囲まれている場合は文の修正
+ *
+ * 行アラインメントを揃えるために1,2のケースは修正されていない方に空行を挿入する。
+ *
+ */
 export const getSplitLineGroups = (lines: string[]) => {
   const originalLines: string[] = []
   const modifiedLines: string[] = []
 
   lines.forEach((line) => {
     if (isInsertLine(line)) {
-      originalLines.push("[--]")
+      originalLines.push("[--]") // 空行を挿入
       modifiedLines.push(line)
     } else if (isDeleteLine(line)) {
       originalLines.push(line)
-      modifiedLines.push("{++}")
+      modifiedLines.push("{++}") // 空行を挿入
     } else if (isModified(line)) {
       originalLines.push("[--]" + removeAddition(line))
       modifiedLines.push("{++}" + removeDeletion(line))
@@ -22,17 +37,17 @@ export const getSplitLineGroups = (lines: string[]) => {
 }
 
 function isInsertLine(line: string) {
-  return /^\{\+[^\{]*\}$/.test(line)
+  return /^\{\+[^\{]*\}$/.test(line) // {+...+}
 }
 function isDeleteLine(line: string) {
-  return /^\[-[^\[]*\]$/.test(line)
+  return /^\[-[^\[]*\]$/.test(line) // [-...-]
 }
 function isModified(line: string) {
-  return /(\{\+|\[-)/.test(line)
+  return /(\{\+|\[-)/.test(line) // {+ or [- を含むか
 }
 function removeAddition(line: string) {
-  return line.replaceAll(/\{\+[^\{\+]*?\+\}/g, "")
+  return line.replaceAll(/\{\+[^\{\+]*?\+\}/g, "") // {+, +} を削除
 }
 function removeDeletion(line: string) {
-  return line.replaceAll(/\[-[^\[-]*?-\]/g, "")
+  return line.replaceAll(/\[-[^\[-]*?-\]/g, "") // [- , -] を削除
 }
