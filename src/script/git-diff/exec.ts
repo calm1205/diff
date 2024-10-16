c6b39912205a2b9d9095d4332397f805c3282234
calm1205 <authentic.nagi@gmail.com>
2024-10-16 16:55:08 +0900

    feat: diff

--- src/script/git-diff/exec.ts
+++ src/script/git-diff/exec.ts
@@ -5,17 +5,17 @@ import { diffFullPage } from "./diffFullPage"
 const rootDir = getRootPath()
 
 // const dir = "1.no-diff"
-// const dir = "2.word-diff"
+const dir = "2.word-diff"
 // const dir = "3.line-diff"
 // const dir = "4.switch-line-diff"
 // const dir = "5.100-page-diff"
-const dir = "6.large-diff"
+// const dir = "6.large-diff"
 // const dir = "7.page-break-diff"
 // const dir = "8.doc-diff"
 // const dir = "9.eng-diff"
 
 const basePath = `${rootDir}fixtures/${dir}/docx-analyzed.json`
-const targetPath = `${rootDir}fixtures/${dir}/diff-analyzed.json`
+const targetPath = `${rootDir}fixtures/${dir}/pdf-analyzed.json`
 const outPath = `${rootDir}fixtures/${dir}/diff.ts`
 
 /**
--- src/script/git-diff/lib/execGitDiff.ts
+++ src/script/git-diff/lib/execGitDiff.ts
@@ -12,6 +12,10 @@ export const execGitDiff = ({ baseFilePath, targetFilePath }: ExecGitDiff) =>
   new Promise<string>((resolve, reject) => {
     const command = `git diff --word-diff-regex='[^[:space:]]' --unified=9999 --no-index ${baseFilePath} ${targetFilePath}`
 
+    // docker container内で実行する場合の文字化け回避
+    // const command = `git diff --word-diff-regex=$'[^\x80-\xbf][\x80-\xbf]*' --unified=9999 --no-index ${baseFilePath} ${targetFilePath}`
+
+    console.log(command)
     exec(command, (error, stdout, stderr) => {
       if (error && stderr) reject(stderr)
       resolve(stdout)
